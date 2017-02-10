b50941b44cb728e0d2dde53c9eb58a54912ca2db
ikj <mooogen@kmon.co.kr>
2017-02-11 01:02:07 +0900

    changed server connection - use express and path

--- nodejsExpress/app.js
+++ nodejsExpress/app.js
@@ -5,13 +5,12 @@
 //express 모듈로드 후 전역변수 express에 할당
 var express = require('express');
 
+var path = require('path');
+
 //express 인스트턴 app 생성
 var app = express();
 
-//get 요청이 오면 익명함수(콜백함수) 호출
-app.get('/', function (req, res) {
-   res.send('hello world');
-});
+app.use(express.static(path.join(__dirname, 'client')));
 
 //3000번 포트로 연결. 연결이 되면 콜백함수 호출
 app.listen(3000, function () {
